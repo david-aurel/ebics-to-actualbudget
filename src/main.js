9c7af836671aa6d0c7a8c20b1b078f905660f6f0
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-07-02 20:14:48 +0200

    feat: add lambda boilerplate

---
+++ src/main.js
@@ -0,0 +1,13 @@
+module.exports.handler = async (event) => {
+    console.log('Event: ', event);
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: "Lambda ran",
+        }),
+    }
+}
