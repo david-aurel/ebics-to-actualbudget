ea8fdd4654054cec77bf6789b6ec90bdd89c2df0
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-07-03 00:03:20 +0200

    chore: reorganize terraform files

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,24 @@
+# S3 to store the Javascript code for the lambda
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket        = "ebics-to-ynab-lambda"
+  force_destroy = true
+}
+
+data "archive_file" "ebics-to-ynab" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/output/lambda.zip"
+}
+
+resource "aws_s3_object" "ebics-to-ynab" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  key    = "lambda.zip"
+  source = data.archive_file.ebics-to-ynab.output_path
+  etag   = filemd5(data.archive_file.ebics-to-ynab.output_path)
+}
+
+
+# Lambda that will run the Javascript code
 resource "aws_lambda_function" "ebics-to-ynab" {
   function_name = "ebics-to-ynab"
 
