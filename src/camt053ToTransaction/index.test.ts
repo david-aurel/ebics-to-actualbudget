4c63af9e1bb1c8ed6302dabe6c6a7d3cd720d5f8
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-11-18 21:33:55 +0100

    test: add test data

--- src/camt053ToTransaction/index.test.ts
+++ src/camt053ToTransaction/index.test.ts
@@ -3,7 +3,8 @@ import { testData } from '../testData'
 
 test('The `camt053ToTransaction` function converts camt053 JSON into actual-budget transaction objects', () => {
   const results = testData.flatMap((data) =>
-    camt053ToTransaction(JSON.stringify(data))
+    camt053ToTransaction('test-account-id')(JSON.stringify(data))
   )
   expect(results).toMatchSnapshot()
+  expect(results.length).toBe(17)
 })
--- src/testData/index.ts
+++ src/testData/index.ts
@@ -1,1 +1,15 @@
-export const testData = []
+import bank_statement_with_card_debit from './bank-statement-with-card-debit.json'
+import bank_statement_with_standing_order_and_ebill from './bank-statement-with-standing-order-and-ebill.json'
+import bank_statement_with_transfer_credit_and_twint_credit from './bank-statement-with-transfer-credit-and-twint-credit.json'
+import bank_statement_with_transfer_debit from './bank-statement-with-transfer-debit.json'
+import bank_statement_with_twint_debit from './bank-statement-with-twint-debit.json'
+import bank_statement_without_entries from './bank-statement-without-entries.json'
+
+export const testData = [
+  bank_statement_with_card_debit,
+  bank_statement_with_standing_order_and_ebill,
+  bank_statement_with_transfer_credit_and_twint_credit,
+  bank_statement_with_transfer_debit,
+  bank_statement_with_twint_debit,
+  bank_statement_without_entries,
+]
