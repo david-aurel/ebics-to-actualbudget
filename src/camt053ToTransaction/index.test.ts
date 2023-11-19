5843a5a5b9a867eb9360ce6dbe9f597939838375
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-11-19 17:53:13 +0100

    fix: adjust camt053 to actual budget transaction conversion

--- src/camt053ToTransaction/index.test.ts
+++ src/camt053ToTransaction/index.test.ts
@@ -1,9 +1,10 @@
 import { camt053ToTransaction } from '.'
 import { testData } from '../testData'
+import { Camt053Schema } from '../zod/camt053'
 
 test('The `camt053ToTransaction` function converts camt053 JSON into actual-budget transaction objects', () => {
   const results = testData.flatMap((data) =>
-    camt053ToTransaction('test-account-id')(JSON.stringify(data))
+    camt053ToTransaction('test-account-id')(Camt053Schema.parse(data))
   )
   expect(results).toMatchSnapshot()
   expect(results.length).toBe(17)
--- src/camt053ToTransaction/index.ts
+++ src/camt053ToTransaction/index.ts
@@ -1,6 +1,6 @@
 import Big from 'big.js'
 
-import { Camt053Schema } from '../zod/camt053'
+import { Camt053SchemaType } from '../zod/camt053'
 import { Transaction } from '../zod/Transaction'
 import { getPayee } from './getPayee'
 import { getNotes } from './getNotes'
@@ -9,11 +9,10 @@ import { getNotes } from './getNotes'
  * @param data a camt053 object
  * @returns an actual-budget transaction object
  */
-export const camt053ToTransaction = (accountId: string) => (data: string) => {
-  const bankStatement = Camt053Schema.parse(JSON.parse(data))
-  const transactions: Transaction[] =
-    bankStatement.document.bankToCustomerStatement.statement.entries.map(
-      (entry) => {
+export const camt053ToTransaction =
+  (accountId: string) => (data: Camt053SchemaType) => {
+    const transactions: Transaction[] =
+      data.document.bankToCustomerStatement.statement.entries.map((entry) => {
         const amount = new Big(entry.amount.value).times(100).toNumber()
         const payee = getPayee(entry)
         const notes = getNotes(entry)
@@ -25,11 +24,10 @@ export const camt053ToTransaction = (accountId: string) => (data: string) => {
           date: entry.bookingDate,
           payee_name: payee,
           imported_payee: payee,
-          amount: entry.creditDebitIndicator === 'debit' ? amount : -amount,
+          amount: entry.creditDebitIndicator === 'debit' ? -amount : amount,
           cleared: true, // camt053 only includes cleared transactions
           notes,
         }
-      }
-    )
-  return transactions
-}
+      })
+    return transactions
+  }
